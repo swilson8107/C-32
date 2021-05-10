76b336f9c8f3398ab5d86d18e426107b03051736
Ellen Wilson <wilsonladies@MacBook-Pro.local>
2021-05-10 16:06:47 -0400

    API Calls

--- sketch.js
+++ sketch.js
@@ -9,9 +9,11 @@ var backgroundImg,platform;
 var bird, slingshot;
 
 var gameState = "onSling";
-
+var bg="sprites/bg.png"
+var score=0
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    //backgroundImg = loadImage("sprites/bg.png");
+    getbackgroundimage()
 }
 
 function setup(){
@@ -45,18 +47,25 @@ function setup(){
 }
 
 function draw(){
+    if(backgroundImg)
     background(backgroundImg);
+    noStroke()
+    textSize(35)
+    fill ("white")
+text("Score:"+score,width-300,50)
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score()
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score()
     log3.display();
 
     box5.display();
@@ -83,6 +92,20 @@ function mouseReleased(){
 
 function keyPressed(){
     if(keyCode === 32){
-       // slingshot.attach(bird.body);
+       slingshot.attach(bird.body);
     }
+}
+async function getbackgroundimage(){
+    var response=await fetch("http://worldtimeapi.org/api/timezone/Asia/Tokyo")
+    var responsejson=await response.json()
+    console.log(responsejson)
+    var datetime=responsejson.datetime
+    var hour=datetime.slice(11,13)
+    if(hour>=03&&hour<=19){
+    bg="sprites/bg.png"    
+    }
+    else{
+        bg="sprites/bg2.jpg"
+    }
+    backgroundImg=loadImage(bg)
 }
